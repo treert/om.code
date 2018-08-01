bea6eec3e5c8c103cb28080ebd985e8562eb9b0b
onemore <treertzhu@123u.com>
2018-08-01 17:56:52 +0800

    [add] 用来写测试代码的工程

---
+++ windows/VsProjectForTest/ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = File.Exists(@"E:\MyGit\github\script_utils\c#\other");
+            var b2 = Directory.Exists(@"E:\MyGit\github\script_utils\c#\other");
+            var b3 = File.Exists(@"E:\MyGit\github\script_utils\c#\other\Readme");
+            var b4 = Directory.Exists(@"E:\MyGit\github\script_utils\c#\other\Readme");
+            Console.WriteLine($"{b1} {b2} {b3} {b4}");
+        }
+    }
+}
