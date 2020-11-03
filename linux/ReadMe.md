e4433cff03ac2a47973aa45e0552187c0608c90f
onemore <909413016@qq.com>
2020-11-03 21:46:25 +0800

    install.shell.env.sh

---
+++ linux/ReadMe.md
@@ -0,0 +1,2 @@
+## 快速部署shell环境
+命令：`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/treert/om.code/master/linux/install.shell.env.sh)"`
---
+++ linux/install.shell.env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~
+
+curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.bash_profile
+curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.bash_rc
+curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.vimrc
