2455d17f1f144d6aca80b636a61a8ce27494ebe6
onemore <909413016@qq.com>
2020-11-05 14:27:31 +0800

    fix install.shell.env.sh

--- linux/install.shell.env.sh
+++ linux/install.shell.env.sh
@@ -3,5 +3,5 @@
 cd ~
 
 curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.bash_profile
-curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.bash_rc
+curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.bashrc
 curl -O https://raw.githubusercontent.com/treert/om.code/master/linux/.vimrc
