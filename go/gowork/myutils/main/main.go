4bb133c21c05b16a389fb1d3ec4f424f7216d548
onemore <909413016@qq.com>
2025-10-14 11:42:49 +0800

    test main.go

--- go/gowork/myutils/main/main.go
+++ go/gowork/myutils/main/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	fmt.Println("go run myutils/main")
+	var s = "123456"
+	// var bb = []byte(s)
+	var bb = unsafe.Slice(unsafe.StringData(s), len(s))
+	fmt.Printf("%p %p %p %p\n", unsafe.SliceData(bb), unsafe.StringData(s), &bb, &s)
+	bb[0] = 'a'
+	fmt.Printf("%p %p %p %p\n", unsafe.SliceData(bb), unsafe.StringData(s), &bb, &s)
+	fmt.Println(string(bb), s)
 }
